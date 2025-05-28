4b8b2ce7aab8b2f7d9be827ee94178f960363746
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-05-28 14:23:11 +0300

    Replace welcome view with home, add about and contact views

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ua">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Контакти</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-xl text-amber-500">Контакти</h1>
+</body>
+
+</html>
