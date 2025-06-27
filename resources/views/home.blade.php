0633c22f13e92b404e86b958fdb113f9657794eb
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-06-27 14:17:33 +0300

    Update home page content: correct dealership location and remove redundant buttons for improved clarity and design.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,25 +4,12 @@
         <x-Text.title>Про нас</x-Text.title>
         <x-Text.subtitle>Офіційний дилер автомобілів ŠKODA в м. Кременчук</x-Text.subtitle>
         <x-Text.text>
-            Ми є офіційним дилером автомобілів ŠKODA в Україні, що надає широкий спектр послуг, включаючи продаж нових
+            Ми є офіційним дилером автомобілів ŠKODA в м. Кременчук, що надає широкий спектр послуг, включаючи продаж
+            нових
             та вживаних автомобілів, сервісне обслуговування, ремонт та продаж оригінальних запчастин. Наша мета -
             забезпечити найвищу якість обслуговування та задоволення потреб наших клієнтів. Ми прагнемо стати вашим
             надійним партнером у світі автомобілів ŠKODA, пропонуючи
             професійний підхід, індивідуальний сервіс та конкурентні ціни.
         </x-Text.text>
-        <div class="flex flex-col md:flex-row gap-4 justify-center items-center">
-            <x-button style="emerald-white" size="md" fullWidth="false">
-                Дізнатися більше
-            </x-button>
-            <x-button style="emerald" size="md" fullWidth="false">
-                Дізнатися більше
-            </x-button>
-            <x-button style="electric" size="md" fullWidth="false">
-                Дізнатися більше
-            </x-button>
-            <x-button style="outline" size="md" fullWidth="false">
-                Дізнатися більше
-            </x-button>
-        </div>
     </x-section>
 </x-layout>
