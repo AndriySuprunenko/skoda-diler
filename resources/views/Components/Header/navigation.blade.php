a6035cf5414d56b1284de6c4ed10e8e35df64f98
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-06-30 12:26:18 +0300

    Fix dropdown arrow alignment: remove unnecessary margin for improved visual consistency in navigation component.

--- resources/views/Components/Header/navigation.blade.php
+++ resources/views/Components/Header/navigation.blade.php
@@ -8,7 +8,7 @@
         <li class="relative dropdown">
             <x-Header.nav-link-def href="#" class="dropdown-toggle flex cursor-pointer group">
                 Послуги
-                <svg class="w-4 h-4 dropdown-arrow transition-transform mt-1" fill="none" stroke="currentColor"
+                <svg class="w-4 h-4 dropdown-arrow transition-transform" fill="none" stroke="currentColor"
                     viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                 </svg>
@@ -28,7 +28,7 @@
         <li class="relative dropdown">
             <x-Header.nav-link-def href="#" class="dropdown-toggle group flex">
                 Моделі
-                <svg class="w-4 h-4 dropdown-arrow transition-transform mt-1" fill="none" stroke="currentColor"
+                <svg class="w-4 h-4 dropdown-arrow transition-transform" fill="none" stroke="currentColor"
                     viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                 </svg>
