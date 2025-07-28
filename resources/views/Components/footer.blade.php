2888d2a4e67b4d3314296a430bca1965aa805e8b
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-07-28 16:00:37 +0300

    add privacy-policy

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -1,7 +1,10 @@
 <footer
     class="bg-skoda-emerald-green px-2 md:px-8 py-4 flex justify-between items-center z-10 flex-col md:flex-row gap-4">
     <x-Header.logo />
+    <a href="{{ asset('privacy-policy.pdf') }}" target="_blank" class="text-skoda-electric-green cursor-pointer">
+        Політика конфіденційності
+    </a>
     <x-text color="electric-green">
-        &copy; {{ date('Y') }} Škoda. All rights reserved.
+        &copy; {{ date('Y') }} Škoda. Всі права захищені.
     </x-text>
 </footer>
